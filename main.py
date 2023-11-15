18c9bbac2119fc0869a41b6ad1a914bb6e4aebad
b1gp <lorenzopavesi00@gmail.com>
2023-11-15 12:33:37 +0100

    Handling division by zero, better UI

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
+
+import os
+
+
+def clear():
+    # controllo la famiglia del sistema operativo per utilizzare la syscall corretta
+    if os.name == 'nt':
+        #per windows
+        _ = os.system('cls')
+    else:
+        #per i sistemi posix compliant
+        _ = os.system('clear')
+
+
 def main():
-    print('Scegli l\'operazione da effettuare:\n1.Somma\n2.Sottrazione\n3.Moltiplicazione\n4.Divisione')
     while True:
-        request=input('Scelta (per uscire scrivi q): ')
-        if request in ('1','2','3','4'):
+        print('Scegli l\'operazione da effettuare:\n1.Somma\n2.Sottrazione\n3.Moltiplicazione\n4.Divisione\nq.Uscita')
+        request = input('Scelta: ')
+        if request in ('1', '2', '3', '4'):
             try:
                 num1 = float(input('Inserisci il primo numero: '))
                 num2 = float(input('Inserisci il secondo numero: '))
             except:
-                print('Input non valido, riprova')
+                clear()
+                print('Input non valido, riprova...')
                 continue
             if request == '1':
-                print(num1, '+', num2, '=', num1+num2)
+                print(num1, '+', num2, '=', num1 + num2)
             elif request == '2':
-                print(num1, '-', num2, '=', num1-num2)
+                print(num1, '-', num2, '=', num1 - num2)
             elif request == '3':
-                print(num1, '*', num2, '=', num1*num2)
+                print(num1, '*', num2, '=', num1 * num2)
             elif request == '4':
-                print(num1, '/', num2, '=', num1/num2)
+                try:
+                    print(num1, '/', num2, '=', num1 / num2)
+                except:
+                    print('Divisione per zero, operazione non consentita')
         elif request == 'q':
             break
         else:
             print('Selezione non valida, riprova')
+        input()
+        clear()
 
 
 # Execute
